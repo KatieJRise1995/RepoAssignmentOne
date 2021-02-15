906d7146011633f7d9d6616d13e877a75a5e7641
Katie Johnston <katiejohnston@Katies-MacBook-Pro.local>
2021-02-14 18:39:43 -0600

    Activity 3 Completed

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var textMessage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textMessage.text?.uppercased()
     }
 }
 
